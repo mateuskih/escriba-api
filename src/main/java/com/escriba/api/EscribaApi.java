1a1863ac09f24f982d1397b4aafbb59187469221
Mateus <mateusoliveirdesouza@hotmail.com>
2024-06-05 04:42:57 -0300

    feat: setup base project

---
+++ src/main/java/com/escriba/api/EscribaApi.java
@@ -0,0 +1,13 @@
+package com.escriba.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EscribaApi {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EscribaApi.class, args);
+	}
+
+}
