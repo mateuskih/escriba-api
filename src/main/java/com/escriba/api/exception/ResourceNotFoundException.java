be0544739072a7a44247ef379a0ada6c18876a4f
Mateus <mateusoliveirdesouza@hotmail.com>
2024-06-05 04:44:17 -0300

    feat: create exceptions

---
+++ src/main/java/com/escriba/api/exception/ResourceNotFoundException.java
@@ -0,0 +1,13 @@
+package com.escriba.api.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public ResourceNotFoundException(String msg) {
+        super(msg);
+    }
+}
