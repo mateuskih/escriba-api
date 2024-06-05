9c8da45975b545d83dc3edb8ef6f309c5736b71c
Mateus <mateusoliveirdesouza@hotmail.com>
2024-06-05 11:54:21 -0300

    db: create migration cartorio_atributo

---
+++ src/main/resources/db/migration/V4__create-table-cartorio_atribuicao.sql
@@ -0,0 +1,7 @@
+CREATE TABLE cartorio_atribuicao (
+    cartorio_id INT NOT NULL,
+    atribuicao_id VARCHAR(20) NOT NULL,
+    PRIMARY KEY (cartorio_id, atribuicao_id),
+    CONSTRAINT fk_cartorio FOREIGN KEY (cartorio_id) REFERENCES cartorios(id),
+    CONSTRAINT fk_atribuicao FOREIGN KEY (atribuicao_id) REFERENCES atribuicoes(id)
+);
