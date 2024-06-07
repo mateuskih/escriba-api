15862d582927d3270d0d70474d3b251345598a52
Mateus <mateusoliveirdesouza@hotmail.com>
2024-06-07 01:38:46 -0300

    db: create insert setup atribuicoes

---
+++ src/main/resources/db/migration/V6__insert-into_atribuicoes.sql
@@ -0,0 +1,7 @@
+INSERT INTO atribuicoes(id, nome) VALUES('ATR_CASAMENTOS', 'Registro de Casamentos');
+INSERT INTO atribuicoes(id, nome) VALUES('ATR_NASCIMENTOS', 'Registro de Nascimentos');
+INSERT INTO atribuicoes(id, nome) VALUES('ATR_OBITOS', 'Registro de Óbitos');
+INSERT INTO atribuicoes(id, nome) VALUES('ATR_PROTESTOS', 'Registro de Protestos');
+INSERT INTO atribuicoes(id, nome) VALUES('ATR_CONTRATOS', 'Registro de Contratos');
+INSERT INTO atribuicoes(id, nome) VALUES('ATR_TESTAMENTOS', 'Registro de Testamentos');
+INSERT INTO atribuicoes(id, nome) VALUES('ATR_INVENTARIOS', 'Inventários e Partilhas');
