3e0073463b96417a704e8b00d9d3762448229e0b
Mateus <mateusoliveirdesouza@hotmail.com>
2024-06-07 01:38:36 -0300

    db: create insert setup cartorios

---
+++ src/main/resources/db/migration/V7__insert-into_cartorios.sql
@@ -0,0 +1,8 @@
+INSERT INTO cartorios(id, nome, observacao, situacao_id) VALUES(1, 'Cartório da Esperança', 'Especializado em registros de nascimentos e casamentos.', 'SIT_ATIVO');
+INSERT INTO cartorios(id, nome, observacao, situacao_id) VALUES(2, 'Cartório do Futuro', 'Focado em serviços digitais e modernos.', 'SIT_ATIVO');
+INSERT INTO cartorios(id, nome, observacao, situacao_id) VALUES(3, 'Cartório Paz e Amor', 'Oferece serviços personalizados para casamentos e nascimentos.', 'SIT_ATIVO');
+INSERT INTO cartorios(id, nome, observacao, situacao_id) VALUES(4, 'Cartório Justiça', 'Especializado em processos judiciais e contratos.', 'SIT_ATIVO');
+INSERT INTO cartorios(id, nome, observacao, situacao_id) VALUES(5, 'Cartório Legalidade', 'Focado em registros de óbitos e protestos.', 'SIT_ATIVO');
+INSERT INTO cartorios(id, nome, observacao, situacao_id) VALUES(6, 'Cartório Universal', 'Oferece todos os tipos de registros e serviços notariais.', 'SIT_ATIVO');
+INSERT INTO cartorios(id, nome, observacao, situacao_id) VALUES(7, 'Cartório da Família', 'Especializado em inventários, partilhas e testamentos.', 'SIT_ATIVO');
+INSERT INTO cartorios(id, nome, observacao, situacao_id) VALUES(8, 'Cartório do Cidadão', 'Focado em serviços para o cidadão comum.', 'SIT_ATIVO');
